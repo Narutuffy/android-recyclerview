173e3097d180cf089bad4939502da8890cfeeeba
Vamsi <s.vamsirao@gmail.com>
2017-05-25 02:55:50 +0530

    Add ItemDecoration

--- app/src/main/java/com/vamsi/recyclerview/MainActivity.java
+++ app/src/main/java/com/vamsi/recyclerview/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
       /*  RecyclerView.LayoutParams lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         mRecyclerView.setLayoutParams(lp);*/
         layoutManager=new GridLayoutManager(this,1, GridLayoutManager.HORIZONTAL, false);
+
         setContentView(mRecyclerView);
         mRecyclerView.setLayoutManager(layoutManager);
 
@@ -33,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         mAdapter=new RecyclerViewAdapter(myClickListener);
-
+        RecyclerView.ItemDecoration itemDecoration =
+                new DividerItemDecoration(this, GridLayoutManager.HORIZONTAL);
+        mRecyclerView.addItemDecoration(itemDecoration);
        mRecyclerView.setAdapter(mAdapter);
 
     }
